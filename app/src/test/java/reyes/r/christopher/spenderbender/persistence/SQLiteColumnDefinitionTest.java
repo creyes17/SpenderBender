6dc098f5ed731d3bf0d1616ac4b0a034c338e789
Chris Reyes <creyes@athenahealth.com>
2016-08-16 11:55:42 -0700

    Starting to test column definitions

--- app/src/test/java/reyes/r/christopher/spenderbender/persistence/SQLiteColumnDefinitionTest.java
+++ app/src/test/java/reyes/r/christopher/spenderbender/persistence/SQLiteColumnDefinitionTest.java
@@ -20,15 +20,84 @@ package reyes.r.christopher.spenderbender.persistence;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 /**
  * Created by Christopher R Reyes on 8/15/16.
  */
 public class SQLiteColumnDefinitionTest {
+    private HashMap<String, SQLiteColumnDefinition.DataType> structuredTypeBySqlType = new HashMap<>();
+
+    @Before
+    public void setUp() throws Exception {
+        // Create a mapping from SQLite Data type to our structured representation
+        // See https://www.sqlite.org/datatype3.html
+
+        // NOTE: This would be much better by using an actual parsing library for SQLite since this list is not complete
+
+        // SQLite Integer Types
+        structuredTypeBySqlType.put("INT", SQLiteColumnDefinition.DataType.INT);
+        structuredTypeBySqlType.put("INTEGER", SQLiteColumnDefinition.DataType.INT);
+        structuredTypeBySqlType.put("TINYINT", SQLiteColumnDefinition.DataType.INT);
+        structuredTypeBySqlType.put("SMALLINT", SQLiteColumnDefinition.DataType.INT);
+        structuredTypeBySqlType.put("MEDIUMINT", SQLiteColumnDefinition.DataType.INT);
+        structuredTypeBySqlType.put("BIGINT", SQLiteColumnDefinition.DataType.INT);
+        structuredTypeBySqlType.put("UNSIGNED BIG INT", SQLiteColumnDefinition.DataType.INT);
+        structuredTypeBySqlType.put("INT2", SQLiteColumnDefinition.DataType.INT);
+        structuredTypeBySqlType.put("INT8", SQLiteColumnDefinition.DataType.INT);
+
+        // SQLite Text types
+        structuredTypeBySqlType.put("CHARACTER", SQLiteColumnDefinition.DataType.TEXT);
+        structuredTypeBySqlType.put("VARCHAR", SQLiteColumnDefinition.DataType.TEXT);
+        structuredTypeBySqlType.put("VARYING CHARACTER", SQLiteColumnDefinition.DataType.TEXT);
+        structuredTypeBySqlType.put("NCHAR", SQLiteColumnDefinition.DataType.TEXT);
+        structuredTypeBySqlType.put("NATIVE CHARACTER", SQLiteColumnDefinition.DataType.TEXT);
+        structuredTypeBySqlType.put("NVARCHAR", SQLiteColumnDefinition.DataType.TEXT);
+        structuredTypeBySqlType.put("TEXT", SQLiteColumnDefinition.DataType.TEXT);
+        structuredTypeBySqlType.put("CLOB", SQLiteColumnDefinition.DataType.TEXT);
+
+        // SQLite Blob types
+        //structuredTypeBySqlType.put("BLOB", SQLiteColumnDefinition.DataType.BLOB);
+
+        // SQLite Real types
+        structuredTypeBySqlType.put("REAL", SQLiteColumnDefinition.DataType.REAL);
+        structuredTypeBySqlType.put("DOUBLE", SQLiteColumnDefinition.DataType.REAL);
+        structuredTypeBySqlType.put("DOUBLE PRECISION", SQLiteColumnDefinition.DataType.REAL);
+        structuredTypeBySqlType.put("FLOAT", SQLiteColumnDefinition.DataType.REAL);
+
+        // SQLite Numeric types
+        //structuredTypeBySqlType.put("NUMERIC", SQLiteColumnDefinition.DataType.NUMERIC);
+        //structuredTypeBySqlType.put("DECIMAL", SQLiteColumnDefinition.DataType.NUMERIC);
+        //structuredTypeBySqlType.put("BOOLEAN", SQLiteColumnDefinition.DataType.NUMERIC);
+        //structuredTypeBySqlType.put("DATE", SQLiteColumnDefinition.DataType.NUMERIC);
+        //structuredTypeBySqlType.put("DATETIME", SQLiteColumnDefinition.DataType.NUMERIC);
+    }
+
     @Test
     public void test_toString() throws Exception {
 
+        // toString should yield a column definition for use in a SQLite Create Table statement
+
+        // Test basic column: just name and type
+        String name1 = "foo";
+        SQLiteColumnDefinition.DataType realType = SQLiteColumnDefinition.DataType.REAL;
+
+        SQLiteColumnDefinition realName1 = new SQLiteColumnDefinition(name1, realType);
+
+        // Statement should be "[name1] [real type]"
+        String statement = realName1.toString();
+
+        /* TODO: This is just for reference while I finish writing these tests
+         * @param name - The name of the column
+         * @param type - The DataType of the column
+         * @param isPrimaryKey - Is this column the primary key for the table?
+         * @param doesAutoIncrement - Should this column auto-increment? (Only applies to primary keys)
+         * @param isUnique - Should there be a default unique constraint on this column?
+         * @param isNotNull - Is this a required column?
+         */
     }
 
     @Test
